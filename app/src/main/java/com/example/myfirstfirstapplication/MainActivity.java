ef7b55098da96466e35646f039d01333a3e5f9d1
Rup-Royofficial <rupayanroy981@gmail.com>
2022-12-23 08:50:31 +0530

    First_commit

---
+++ app/src/main/java/com/example/myfirstfirstapplication/MainActivity.java
@@ -0,0 +1,16 @@
+//source code of the file
+
+package com.example.myfirstfirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
